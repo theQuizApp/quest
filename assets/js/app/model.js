233b9bb2e548959eff60383b30977ba736551004
amolks <kshirsagar.amol.r@gmail.com>
2014-09-04 01:21:27 +0100

    updated modal

--- assets/js/app/model.js
+++ assets/js/app/model.js
@@ -1,14 +1,39 @@
 var Question = Backbone.Model.extend({
         defaults: {
+            section: {
+                quant:{
+                    ds:'ds',
+                    ps:'ps'
+                },
+                verbal:{
+                    sc:'sc',
+                    rc:'rc',
+                    cr:'cr'
+                }
+            },
             question: 'how are you',
-            ans: 'asas',
+            ans: {
+                options: {
+                    a:true,
+                    b:false,
+                    c:false,
+                    d:false,
+                    e:false,
+                    all:false
+                }
+            },
             time: 0,
             options: {
-            	a:'a',
-            	b:'b',
-            	c:'c',
-            	d:'d',
-            	e:'e'
+                a:'what',
+                b:'how',
+                c:'when',
+                d:'where',
+                e:'which'
+            },
+            dificulty: {
+                high:false,
+                medium:true,
+                easy:false
             }
         },
         initialize: function(){
